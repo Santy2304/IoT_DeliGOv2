29c589159c2e1b752c13a45e669e28e08f4b00e5
Hineill <hineill.cespedes@pucp.edu.pe>
2024-09-28 01:09:46 -0500

    Agregando cosas

---
+++ app/src/main/java/com/example/deligov2/LogIn/LoginCrearCuentaTercerPaso.java
@@ -0,0 +1,26 @@
+package com.example.deligov2.LogIn;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.deligov2.R;
+
+public class LoginCrearCuentaTercerPaso extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login_crear_cuenta_tercer_paso);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
