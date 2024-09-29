31c96618d6975195e5228e3b2c61c970b0319482
J3anP <a20213733@pucp.edu.pe>
2024-09-28 23:25:40 -0500

    prueba añadir, solo estoy añadiendo poco, no todo y los beans

---
+++ app/src/main/java/com/example/deligov2/Beans/Administrador.java
@@ -0,0 +1,76 @@
+package com.example.deligov2.Beans;
+
+public class Administrador {
+    private int idAdmin;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private boolean estado;
+    private String restaurante;
+    private String ubicacionRestaurante;
+    private String numDocumento;
+
+    public int getIdAdmin() {
+        return idAdmin;
+    }
+
+    public void setIdAdmin(int idAdmin) {
+        this.idAdmin = idAdmin;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public String getRestaurante() {
+        return restaurante;
+    }
+
+    public void setRestaurante(String restaurante) {
+        this.restaurante = restaurante;
+    }
+
+    public String getUbicacionRestaurante() {
+        return ubicacionRestaurante;
+    }
+
+    public void setUbicacionRestaurante(String ubicacionRestaurante) {
+        this.ubicacionRestaurante = ubicacionRestaurante;
+    }
+
+    public String getNumDocumento() {
+        return numDocumento;
+    }
+
+    public void setNumDocumento(String numDocumento) {
+        this.numDocumento = numDocumento;
+    }
+}
---
+++ app/src/main/java/com/example/deligov2/Beans/Repartidor.java
@@ -0,0 +1,86 @@
+package com.example.deligov2.Beans;
+
+public class Repartidor {
+
+    private int idRepartidor;
+    private String nombre;
+    private String apellido;
+    private boolean estado;
+    private boolean estadoSolicitud;
+    private String numDocument;
+    private String correo;
+    private String domicilio;
+    private String numTelefono;
+
+    public int getIdRepartidor() {
+        return idRepartidor;
+    }
+
+    public void setIdRepartidor(int idRepartidor) {
+        this.idRepartidor = idRepartidor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public boolean isEstadoSolicitud() {
+        return estadoSolicitud;
+    }
+
+    public void setEstadoSolicitud(boolean estadoSolicitud) {
+        this.estadoSolicitud = estadoSolicitud;
+    }
+
+    public String getNumDocument() {
+        return numDocument;
+    }
+
+    public void setNumDocument(String numDocument) {
+        this.numDocument = numDocument;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getNumTelefono() {
+        return numTelefono;
+    }
+
+    public void setNumTelefono(String numTelefono) {
+        this.numTelefono = numTelefono;
+    }
+}
