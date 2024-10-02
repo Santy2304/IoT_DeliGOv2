7ac62634c050d8209eda88f23b9af0246db1dba6
Hineill <hineill.cespedes@pucp.edu.pe>
2024-10-02 03:02:35 -0500

    Desplazamiento de tiempo

--- app/src/main/java/com/example/deligov2/Repartidor/RepartidorTrackingEstadoRecibido.java
+++ app/src/main/java/com/example/deligov2/Repartidor/RepartidorTrackingEstadoRecibido.java
@@ -2,6 +2,7 @@ package com.example.deligov2.Repartidor;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
@@ -24,10 +25,33 @@ public class RepartidorTrackingEstadoRecibido extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Iniciar la nueva actividad
+                Intent intent = new Intent(RepartidorTrackingEstadoRecibido.this, RepartidorTrackingEstadoEnPreparacion.class);
+                startActivity(intent);                // Finalizar la actividad actual (opcional)
+                finish();
+            }
+        }, 1000 * 20); //60 segundos
+
+
+
     }
 
-    public void verSiguienteTracking(View view){
-        Intent intent = new Intent(this, RepartidorTrackingEstadoEnPreparacion.class);
+    public void  retroceder(View view)
+    {
+        onBackPressed();
+    }
+
+    public void verNotificacionesRepartidor(View view){
+        Intent intent = new Intent(this, RepartidorNotificaciones.class);
+        startActivity(intent);
+    }
+
+    public void verPerfil(View view){
+        Intent intent = new Intent(this, PerfilRepartidor.class);
         startActivity(intent);
     }
 }
