89b033318294f542022b55aa7ee43c2435f6b575
Hineill <hineill.cespedes@pucp.edu.pe>
2024-10-23 18:45:55 -0500

    Holaaa

--- app/src/main/java/com/example/deligov2/LogIn/LoginInicioSesion.java
+++ app/src/main/java/com/example/deligov2/LogIn/LoginInicioSesion.java
@@ -2,6 +2,7 @@ package com.example.deligov2.LogIn;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
@@ -11,7 +12,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 
+import com.example.deligov2.Administrador.AdministradorHomeActivity;
+import com.example.deligov2.Cliente.ClienteHomeActivity;
 import com.example.deligov2.R;
+import com.example.deligov2.Repartidor.RepartidorVistaHome;
+import com.example.deligov2.SuperAdmin.SuperAdminHomeActivity;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginInicioSesion extends AppCompatActivity {
 
@@ -38,5 +45,76 @@ public class LoginInicioSesion extends AppCompatActivity {
         Intent intent = new Intent(LoginInicioSesion.this, LoginCrearCuentaPrimerPaso.class);
         startActivity(intent);//Sin destruir el activity;
     }
+
+    public void iniciarSesion(View view){
+        //BUSCAMOS LOS VALORES DE LOS CAMPOS
+        TextInputEditText email = findViewById(R.id.correo);
+        TextInputEditText password = findViewById(R.id.password);
+        String emailStr =  email.getText().toString();
+        String passwordStr =  password.getText().toString();
+        int counter = 0 ;
+
+        if(emailStr.equals("admin") && passwordStr.equals("admin")){
+            startActivity(new Intent(this, AdministradorHomeActivity.class));
+        }else{
+            counter++;
+        }
+
+        if(emailStr.equals("superAdmin") && passwordStr.equals("superAdmin")){
+            startActivity(new Intent(this, SuperAdminHomeActivity.class));
+        }else{
+            counter++;
+        }
+
+        if(emailStr.equals("cliente") && passwordStr.equals("cliente")){
+            startActivity(new Intent(this, ClienteHomeActivity.class));
+        }else{
+            counter++;
+        }
+
+        if(emailStr.equals("repartidor") && passwordStr.equals("repartidor")){
+            startActivity(new Intent(this, RepartidorVistaHome.class));
+        }else{
+            counter++;
+        }
+
+        if(counter==4){
+            //ERROR
+            validateFields(email,  password, findViewById(R.id.correoLayout), findViewById(R.id.passwordLayout) );
+        }
+
+
+
+    }
+
+
+    private boolean validateFields(TextInputEditText correoEditText , TextInputEditText contrasenaEditText , TextInputLayout correoLayout  , TextInputLayout contrasenaLayout ) {
+        String correo = correoEditText.getText().toString().trim();
+        String contrasena = contrasenaEditText.getText().toString().trim();
+        boolean isValid = true;
+
+        // Validar campo de correo
+        if (correo.isEmpty()) {
+            correoLayout.setError("El campo de correo no puede estar vacío");
+            isValid = false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(correo).matches()) {
+            correoLayout.setError("Por favor, introduce un correo válido");
+            isValid = false;
+        } else {
+            correoLayout.setError(null); // Limpiar error si es válido
+        }
+
+        // Validar campo de contraseña
+        if (contrasena.isEmpty()) {
+            contrasenaLayout.setError("El campo de contraseña no puede estar vacío");
+            isValid = false;
+        } else {
+            contrasenaLayout.setError(null); // Limpiar error si es válida
+        }
+
+        return isValid;
+    }
+
+
 
 }
