f85236ac7ba09ee74304f7689c985c156d27f775
Yosthim <a20201497@pucp.edu.pe>
2024-12-01 00:33:08 -0500

    Avance

--- app/src/main/java/com/example/deligov2/Administrador/AdministradorRegistroPlato1Activity.java
+++ app/src/main/java/com/example/deligov2/Administrador/AdministradorRegistroPlato1Activity.java
@@ -5,14 +5,49 @@ import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.deligov2.DTO.Usuario;
 import com.example.deligov2.R;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class AdministradorRegistroPlato1Activity extends AppCompatActivity {
+
+    TextInputEditText eTnombrePlato, eTprecioPlato, eTdescripcionPlato;
+    FirebaseFirestore db;
+    FirebaseUser user;
+    FirebaseAuth auth;
+    String idRestaurante;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_administrador_registro_plato_parte1);
+
+        // Inicializar instancias de Firebase
+        db = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+        // Obtener referencias a los campos de entrada de texto
+        eTnombrePlato = findViewById(R.id.nombrePlato);
+        eTprecioPlato = findViewById(R.id.precioPlato);
+        eTdescripcionPlato = findViewById(R.id.descripcionPlato);
+
+        //Obtener el usuario actual (administrador)
+        user = auth.getCurrentUser();
+        if (user != null) {
+            String uid = user.getUid();
+            db.collection("Usuarios").document(uid).get()
+                    .addOnSuccessListener(documentSnapshot -> {
+                        Usuario usuario = documentSnapshot.toObject(Usuario.class);
+                        assert usuario != null : "Usuario no creado en DB";
+                        idRestaurante = usuario.getRestaurante();
+                    })
+                    .addOnFailureListener(e -> {
+                        // Manejar el error al obtener los datos del usuario
+                    });
+        }
+
 
     }
 }
